0a5aae620cdf060e4ea5a2107b482071e210e2a4
Pavel Solin <solin.pavel@gmail.com>
2016-03-13 20:55:46 -0700

    Update

--- src/pyplasm/fenvs.py
+++ src/pyplasm/fenvs.py
@@ -9941,6 +9941,8 @@ class NCLabTurtle3D:
         dyref4 = sin(alpha) * dxref3 + cos(alpha) * dyref3
         dzref4 = dzref3
         print("New unit vector:", round(dxref4, 3), round(dyref4, 3), round(dzref4, 3))
+        # First let's adjust roll angle:
+        self.turtleangle3 += sin(da1) * self.turtleangle2
         # We have the global vector, now calculate the new angles:
         dist = sqrt(dxref4**2 + dyref4**2)
         self.turtleangle2 = arctan2(dzref4, dist) * 180 / pi
