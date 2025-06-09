0d8f779a0cdc2d5f7810605947ac01002fe3873f
Alexander Freyer <afreyer@nclab.com>
2025-06-09 16:26:08 +0200

    DEV-5294-include-opacity-for-pyplasm: add opacity and shininess support to objects
    Enhanced BASEOBJ class and COLOR function to support material properties:
    - Add opacity and shininess attributes to BASEOBJ constructor
    - Update VIEWBASE function to pass material properties to visualizer
    - Extend COLOR function with opacity and shininess parameters
    - Include validation for opacity and shininess values (0.0-1.0 range)

--- src/xgepy/nclab_pyplasm/operations/color.py
+++ src/xgepy/nclab_pyplasm/operations/color.py
@@ -26,16 +26,32 @@ def color(*args):
     raise ExceptionWT("Command color() is undefined. Try COLOR() instead?")
 
 
-def COLOR(obj, col=None):
+def COLOR(obj, color=None, opacity=None, shininess=None):
     # obj may be a single object or a list of objects
-    if col is None:
+    if color is None:
         raise ExceptionWT(
             "The COLOR command takes two arguments: a 2D or 3D object and a color."
         )
+
+    if opacity is not None and (opacity < 0.0 or opacity > 1.0):
+        raise ExceptionWT("Opacity value must be between 0.0 and 1.0.")
+
+    if isinstance(color, list) and len(color) == 4 and (opacity is not None):
+        raise ExceptionWT(
+            "You are already providing an opacity value in the color list. Please remove the 'opacity' parameter."
+        )
+
+    if shininess is not None and (shininess < 0.0 or shininess > 1.0):
+        raise ExceptionWT("Shininess value must be between 0.0 and 1.0.")
+
     if not isinstance(obj, list):
         if not isinstance(obj, BASEOBJ):
             raise ExceptionWT("The first argument of COLOR must be an object!")
-        obj.setcolor(col)
+        if opacity is not None:
+            object.opacity = opacity
+        if shininess is not None:
+            object.shininess = shininess
+        obj.setcolor(color)
     else:
         obj = flatten(obj)
         for x in obj:
@@ -43,7 +59,11 @@ def COLOR(obj, col=None):
                 raise ExceptionWT("Use the UNION command to create unions of objects.")
             if not isinstance(x, BASEOBJ):
                 raise ExceptionWT("Invalid object found (color - 1).")
-            x.setcolor(col)
+            if opacity is not None:
+                object.opacity = opacity
+            if shininess is not None:
+                object.shininess = shininess
+            x.setcolor(color)
 
 
 C = COLOR  # Short form
--- src/xgepy/nclab_pyplasm/utils/viewbase.py
+++ src/xgepy/nclab_pyplasm/utils/viewbase.py
@@ -12,9 +12,22 @@ from ...fenvs import nclabinst
 
 
 def VIEWBASE(objects):
-    geoms = []
-    for x in objects:
+    """
+    Prepares objects for visualization by packaging geometry, color, and material.
+    """
+    if not isinstance(objects, list):
+        objects = [objects]
+
+    items_for_converter = []
+    for i, x in enumerate(objects):
         if not isinstance(x, BASEOBJ):
             raise ExceptionWT("The arguments must be objects!")
-        geoms.append(x.geom)
-    nclabinst.visualize(nclabinst.converter(geoms))
+
+        obj_geom = x.geom
+        obj_color = x.color
+        obj_opacity = x.opacity
+        obj_shininess = x.shininess
+        item_data = (obj_geom, obj_color, obj_opacity, obj_shininess)
+        items_for_converter.append(item_data)
+
+    nclabinst.visualize(nclabinst.converter(items_for_converter))
